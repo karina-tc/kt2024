7e416f3b8683602a6498eb8dd2a817572d0c6d5b
karina-tc <hello@karinatovar.com>
2024-10-25 21:50:28 -0400

    quality of life improvements

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				'instrument': ['"Instrument Serif"', 'serif'],
+			},
 			colors: {
 				light: {
 					base: '#F9F7F5',
