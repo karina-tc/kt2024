89fdddb1a73c9450917e4bcaf562489568386392
karina-tc <hello@karinatovar.com>
2024-10-26 08:25:40 -0400

    fine tuning showcase image

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import icon from 'astro-icon';
-
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind()],
   output: 'server',
   adapter: netlify()
 });
